f1b96b80a8d186b88000cd9e67bfed7b421fd91f
MorozDS <morozddima@gmail.com>
2022-03-09 23:03:53 +0200

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
-
+import Player from '@vimeo/player';
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+   player.on('play', function() {
+        console.log('played the video!');
+   });
+    
+player.on('timeupdate', function (data) {
+    const jsonDataSeconds = JSON.stringify(data.seconds);
+    console.log(jsonDataSeconds)
+    localStorage.setItem('videoplayer-current-time', jsonDataSeconds)
+    
+   })
