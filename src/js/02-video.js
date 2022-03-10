4bbdc871376a27b9ddf5f0644db1d448006336ca
MorozDS <morozddima@gmail.com>
2022-03-10 22:35:57 +0200

    task 2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,13 +1,35 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
+
    player.on('play', function() {
         console.log('played the video!');
    });
     
-player.on('timeupdate', function (data) {
-    const jsonDataSeconds = JSON.stringify(data.seconds);
-    console.log(jsonDataSeconds)
-    localStorage.setItem('videoplayer-current-time', jsonDataSeconds)
-    
-   })
+player.on('timeupdate', throttle(update, 100));
+
+
+function update(data) {
+   localStorage.setItem('videoplayer-current-time', JSON.stringify(data.seconds))
+
+    };
+
+
+   
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time')).then(function(seconds) {
+  
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
+
+ 
