fdcba8dbd90b9a48eca50a05ef7f73ee7c865f02
MorozDS <morozddima@gmail.com>
2022-03-27 18:50:07 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -16,20 +16,15 @@ function update(data) {
 
     };
 
+const saveTime = localStorage.getItem('videoplayer-current-time');
 
-   
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time')).then(function(seconds) {
-  
-}).catch(function(error) {
-    switch (error.name) {
-        case 'RangeError':
-            // the time was less than 0 or greater than the video’s duration
-            break;
-
-        default:
-            // some other error occurred
-            break;
+function currentTime () {
+    if(saveTime !== null) {
+        player.setCurrentTime(saveTime);
     }
-});
+}
+currentTime();
+
+
 
  
